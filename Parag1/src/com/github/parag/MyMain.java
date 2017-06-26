910e464908219c1464af91f4c461f5b708cdab55
paragmahajan27 <paragmahajan27@gmail.com>
2017-06-26 02:10:38 -0500

    My Project

---
+++ Parag1/src/com/github/parag/MyMain.java
@@ -0,0 +1,13 @@
+package com.github.parag;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		MyMain.print();
+	}
+	
+	public static void print() {
+		System.out.println("This is Parag's GitHub Test");
+	}
+
+}
---
+++ Parag1/test/com/github/parag/MyMainTest.java
@@ -0,0 +1,12 @@
+package com.github.parag;
+
+import org.junit.Test;
+
+public class MyMainTest {
+
+	@Test
+	public void testMyMain() {
+		System.out.println("Running JUnit test testMyMain()");
+		MyMain.print();
+	}
+}
